bfd1d9c09abdb5e626952ab5b48b60d721803f8c
Czarnodziej <pagodemc@gmail.com>
2014-07-05 18:30:41 +0200

    raw contact form + validation

--- src/Czarnodziej/CoreBundle/Controller/DefaultController.php
+++ src/Czarnodziej/CoreBundle/Controller/DefaultController.php
@@ -3,11 +3,45 @@
 namespace Czarnodziej\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Czarnodziej\CoreBundle\Form\Type\ContactType;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('CzarnodziejCoreBundle:Default:index.html.twig');
+        $form = $this->createForm(new ContactType());
+        if ($request->isMethod('POST'))
+        {
+            $form->bind($request);
+
+            if ($form->isValid())
+            {
+                $message = \Swift_Message::newInstance()
+                        ->setSubject($form->get('subject')->getData())
+                        ->setFrom($form->get('email')->getData())
+                        ->setTo('pagodemc@gmail.com')
+                        ->setBody(
+                        $this->renderView(
+                                'CzarnodziejCoreBundle:Mail:contact.html.twig', array(
+                            'ip'      => $request->getClientIp(),
+                            'name'    => $form->get('name')->getData(),
+                            'message' => $form->get('message')->getData()
+                                )
+                        )
+                );
+
+                $this->get('mailer')->send($message);
+
+                $request->getSession()->getFlashBag()->add('success', 'contact.flash.sent');
+
+                return $this->redirect($this->generateUrl('czarnodziej_core_homepage'));
+            }
+        }
+
+        return
+                $this->render('CzarnodziejCoreBundle:Default:index.html.twig', array(
+                    'form' => $form->createView(),)
+        );
     }
 }
---
+++ src/Czarnodziej/CoreBundle/Form/Type/ContactType.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Czarnodziej\CoreBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+//use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+//use Symfony\Component\Validator\Constraints\Collection;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+                ->add('name', 'text', array(
+                    'label'       => 'contact.label.name',
+                    'attr'        => array(
+                        'placeholder' => 'contact.placeholder.name',
+                        'pattern'     => '.{2,}' //minlength
+                    ),
+                    'constraints' => array(
+                        new NotBlank(array('message' => 'contact.name.not_blank')),
+                        new Length(array('min' => 2, 'minMessage'=>'contact.name.min_message')),
+                    )
+                ))
+                ->add('email', 'email', array(
+                    'label'       => 'contact.label.email',
+                    'attr'        => array(
+                        'placeholder' => 'contact.placeholder.email',
+                    ),
+                    'constraints' => array(
+                        new NotBlank(array('message' => 'contact.email.not_blank')),
+                        new Email(array('message' => 'contact.email.valid'))
+                    )
+                ))
+                ->add('subject', 'text', array(
+                    'label'       => 'contact.label.subject',
+                    'attr'        => array(
+                        'placeholder' => 'contact.placeholder.subject',
+                        'pattern'     => '.{3,}' //minlength
+                    ),
+                    'constraints' => array(
+                        new NotBlank(array('message' => 'contact.subject.not_blank')),
+                        new Length(array('min' => 3, 'minMessage'=>'contact.subject.min_message'))
+                    )
+                ))
+                ->add('message', 'textarea', array(
+                    'label'       => 'contact.label.message',
+                    'attr'        => array(
+                        'cols'        => 90,
+                        'rows'        => 10,
+                        'placeholder' => 'contact.placeholder.message'
+                    ),
+                    'constraints' => array(
+                        new NotBlank(array('message' => 'contact.message.not_blank')),
+                        new Length(array('min' => 5, 'minMessage'=>'contact.message.min_message'))
+                    )
+        ));
+    }
+    public function getName()
+    {
+        return 'contact';
+    }
+}
